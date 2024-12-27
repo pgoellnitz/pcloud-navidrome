4dca7eb0ead76992dc9796618079b261d5215f75
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2024-12-27 23:15:30 +0100

    enable experimental 2fa support (#1)

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ sudo docker run -it \
  -v pcloud-docker_pcloud-cache-dir:/root/.pcloud \
  -v pcloud-docker_pcloud-data:/mnt/pcloud-data \
  --name pcloud \
- zcalusic/pcloud \
+ pgoellnitz/pcloud_2fa \
  pcloudcc -u ${PCLOUD_ACCOUNT_EMAIL} -s -m /mnt/pcloud-data
